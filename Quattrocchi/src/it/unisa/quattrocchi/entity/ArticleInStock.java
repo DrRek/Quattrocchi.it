e25c9875344a37e5db901a19511c6d725048e18c
DrRek <lucareccia@hotmail.it>
2018-01-03 23:11:06 +0100

    primi metodi della metà superiore delle entity

--- Quattrocchi/src/it/unisa/quattrocchi/entity/ArticleInStock.java
+++ Quattrocchi/src/it/unisa/quattrocchi/entity/ArticleInStock.java
@@ -1,5 +1,19 @@
 package it.unisa.quattrocchi.entity;
 
-public class ArticleInStock {
+public class ArticleInStock extends Article{
 
+	int disponibilità;
+	
+	public ArticleInStock(String modello, String marca, String img1, String img2, String img3, String descrizione, int disponibilità, double prezzo) {
+		super(modello, marca, img1, img2, img3, descrizione, prezzo);
+		this.disponibilità = disponibilità;
+	}
+
+	public int getDisponibilità() {
+		return disponibilità;
+	}
+
+	public void setDisponibilità(int disponibilità) {
+		this.disponibilità = disponibilità;
+	}
 }

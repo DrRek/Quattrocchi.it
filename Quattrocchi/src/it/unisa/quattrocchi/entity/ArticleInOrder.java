c55988e58711b63905c6d73b659de9e98445cfed
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-12 11:57:47 +0100

    niente

--- Quattrocchi/src/it/unisa/quattrocchi/entity/ArticleInOrder.java
+++ Quattrocchi/src/it/unisa/quattrocchi/entity/ArticleInOrder.java
@@ -4,9 +4,12 @@ public class ArticleInOrder extends Article {
 	
 	int quantità;
 	
+	//si dovrebbe aggiungere l'ordine a cui è associato
+	
 	public ArticleInOrder(String codice, String modello, String marca, String img1, String img2, String img3, String descrizione, double prezzo, int quantità) {
 		super(codice, modello, marca, img1, img2, img3, descrizione, prezzo);
 		this.quantità=quantità;
+		
 	}
 
 	public int getQuantità() {

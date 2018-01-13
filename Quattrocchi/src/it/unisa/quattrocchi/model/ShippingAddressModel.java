999163959094dd8650685474c0f3f9e71ac9551a
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-13 11:49:43 +0100

    Gnais commit
    Shipping address model
    CreditCard Model

--- Quattrocchi/src/it/unisa/quattrocchi/model/ShippingAddressModel.java
+++ Quattrocchi/src/it/unisa/quattrocchi/model/ShippingAddressModel.java
@@ -7,11 +7,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.unisa.quattrocchi.entity.Acquirente;
 import it.unisa.quattrocchi.entity.ShippingAddress;
 
 public class ShippingAddressModel {
 	
 	private static final String TABLE_NAME_ADDRESS = "quattrocchidb.indirizzospedizione";
+	
+	static AcquirenteModel acquirenteModel = new AcquirenteModel();
 	
 	public ShippingAddress doRetrieveById(String idShip) throws SQLException {
 		Connection conn = null;
@@ -35,9 +38,9 @@ public class ShippingAddressModel {
 				int cap = rs.getInt("CAP");
 				String indirizzo = rs.getString("Indirizzo");
 				int nc = rs.getInt("NumeroCivico");
-				//Acquirente acq = userModel.doRetriveById(rs.getString("Acquirente"));
+				Acquirente acq = acquirenteModel.doRetriveById(rs.getString("Acquirente"));
 				
-				bean = new ShippingAddress(codice,stato,indirizzo,cap,provincia,nc,null);
+				bean = new ShippingAddress(codice,stato,indirizzo,cap,provincia,nc,acq);
 			}
 			
 			stm.close();
@@ -74,9 +77,9 @@ public class ShippingAddressModel {
 				int cap = rs.getInt("CAP");
 				String indirizzo = rs.getString("Indirizzo");
 				int nc = rs.getInt("NumeroCivico");
-				//Acquirente acq = userModel.doRetriveById(rs.getString("Acquirente"));
+				Acquirente acq = acquirenteModel.doRetriveById(rs.getString("Acquirente"));
 				
-				beans.add(new ShippingAddress(codice,stato,indirizzo,cap,provincia,nc,null));
+				beans.add(new ShippingAddress(codice,stato,indirizzo,cap,provincia,nc,acq));
 			}
 			stm.close();
 			rs.close();
@@ -92,4 +95,47 @@ public class ShippingAddressModel {
 		
 		return beans;
 	}
+	
+	public void createShippingAddress(ShippingAddress toCreate) throws SQLException{
+		Connection conn = null;
+		PreparedStatement stm = null;
+		
+		String query = "INSERT INTO " + TABLE_NAME_ADDRESS + 
+				" (Id,Stato,Provincia,CAP,Indirizzo,NumeroCivico,Acquirente)"+
+				" VALUES(?,?,?,?,?,?,?);";
+		
+		String id = toCreate.getCodice();
+		String stato = toCreate.getStato();
+		String provincia = toCreate.getProvincia();
+		int cap = toCreate.getCap();
+		String indirizzo = toCreate.getIndirizzo();
+		int numeroCivico = toCreate.getNC();
+		String acquirente = toCreate.getAcq().getUsername();
+		
+		try {
+			conn = DriverManagerConnectionPool.getConnection();
+			stm = conn.prepareStatement(query);
+			
+			stm.setString(1, id);
+			stm.setString(2, stato);
+			stm.setString(3, provincia);
+			stm.setInt(4, cap);
+			stm.setString(5, indirizzo);
+			stm.setInt(6, numeroCivico);
+			stm.setString(7, acquirente);
+			
+			stm.executeUpdate();
+			stm.close();
+			conn.commit();
+		}finally {
+			try {
+				if(stm != null)
+					stm.close();
+			}finally {
+				DriverManagerConnectionPool.releaseConnection(conn);
+			}
+		}
+		return;
+	}
+	
 }

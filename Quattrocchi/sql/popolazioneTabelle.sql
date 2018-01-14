d32c600ad34d32bebb3c8be91d37fe041720bd56
abbettino <albertinopalmiero@gmail.com>
2018-01-14 09:35:04 +0100

    updates

--- Quattrocchi/sql/popolazioneTabelle.sql
+++ Quattrocchi/sql/popolazioneTabelle.sql
@@ -4,39 +4,39 @@
 -- luigi C:\\Users\\luigi\\OneDrive\\Documenti\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\
 
 use quattrocchiDB;
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\Amministratore.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\Amministratore.csv'
 	INTO TABLE Amministratore
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\GestoreOrdini.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\GestoreOrdini.csv'
 	INTO TABLE GestoreOrdini
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\Acquirente.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\Acquirente.csv'
 	INTO TABLE Acquirente
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\IndirizzoSpedizione.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\IndirizzoSpedizione.csv'
 	INTO TABLE IndirizzoSpedizione
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\CartaCredito.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\CartaCredito.csv'
 	INTO TABLE CartaCredito
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\Ordine.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\Ordine.csv'
 	INTO TABLE Ordine
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\ArticoloInOrder.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\ArticoloInOrder.csv'
 	INTO TABLE ArticoloInOrder
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\ArticoloInStock.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\ArticoloInStock.csv'
 	INTO TABLE ArticoloInStock
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();
-LOAD DATA LOCAL INFILE 'D:\\Users\\Luca\\Desktop\\file\\unisa\\Lezioni Ingegneria del Software\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\ArticoloInCarrello.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Alberto\\Documents\\GitHub\\Quattrocchi.it\\Quattrocchi\\sql\\csv\\ArticoloInCarrello.csv'
 	INTO TABLE ArticoloInCarrello
 		FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n'
 	();

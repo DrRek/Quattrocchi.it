6b5abbabb83a4d5761a0690caa847e012a0d3ed2
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-14 10:03:54 +0100

    commit

--- Quattrocchi/src/it/unisa/quattrocchi/control/gestione_ordini/VisualizzaOrdini.java
+++ Quattrocchi/src/it/unisa/quattrocchi/control/gestione_ordini/VisualizzaOrdini.java
@@ -1,19 +1,36 @@
 package it.unisa.quattrocchi.control.gestione_ordini;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
+import it.unisa.quattrocchi.model.OrderModel;
+
 public class VisualizzaOrdini extends HttpServlet{
+	
+	static OrderModel orderModel = new OrderModel();
 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
-		
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		try {
+			response.getWriter().write(new Gson().toJson(orderModel.doRetrieveAll()));
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/web_pages/view/GestoreOrdiniView.jsp");
+		dispatcher.forward(request, response);
 		return;
 	}
 	
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		doGet(request, response);
 		return;
 	}

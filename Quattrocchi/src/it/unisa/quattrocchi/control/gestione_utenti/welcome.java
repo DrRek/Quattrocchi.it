d549132d3ac991d891585f5b63b36a61d95abe6f
DrRek <lucareccia@hotmail.it>
2018-01-15 12:28:47 +0100

    alberto è lento

---
+++ Quattrocchi/src/it/unisa/quattrocchi/control/gestione_utenti/welcome.java
@@ -0,0 +1,32 @@
+package it.unisa.quattrocchi.control.gestione_utenti;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+
+public class welcome extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/web_pages/view/Index.jsp");
+		dispatcher.forward(request, response);	
+			
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		return;
+	}
+}
+

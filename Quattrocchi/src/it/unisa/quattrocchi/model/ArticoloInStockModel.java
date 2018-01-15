a44e74a5c74c252ab1bdb3a0a4a62030fd3ec964
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-15 11:59:54 +0100

    Javadoc ArticoloInStockModel
    Aggiornato odd

--- Quattrocchi/src/it/unisa/quattrocchi/model/ArticoloInStockModel.java
+++ Quattrocchi/src/it/unisa/quattrocchi/model/ArticoloInStockModel.java
@@ -6,14 +6,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import it.unisa.quattrocchi.entity.ArticoloInStock;
 
+
+/**
+ * 
+ * @author quattrocchi.it
+ * Questa classe è un manager che si occupa di interagire con il database.
+ * Gestisce le query riguardanti gli articoli in stock. 
+ */
 public class ArticoloInStockModel {
 	
 	private static final String TABLE_NAME_CATALOGO = "quattrocchidb.articoloinstock";
 
-	
+	/**
+	 * Questo metodo si occupa di verificare se nel database è presente un articolo in stock
+	 * tramite una codice specifico preso in input.
+	 * @param codiceProdotto un oggetto codiceProdotto di tipo <strong>String</strong>
+	 * @return un oggetto di tipo <strong>ArticoloInStock</strong>, altrimenti null.
+	 * @throws SQLException
+	 */
 	public ArticoloInStock doRetrieveByIdInStock(String codiceProdotto) throws SQLException {
 		Connection conn = null;
 		PreparedStatement stm = null;
@@ -57,6 +69,12 @@ public class ArticoloInStockModel {
 	}
 	
 	
+	/**
+	 * Questo metodo si occupa di effettuare la ricerca di tutti gli articoli in stock
+	 * presenti nel database.
+	 * @return una lista di articoli in stock di tipo <strong>ArticoloInStock</strong>, altrimenti null.
+	 * @throws SQLException
+	 */
 	public List<ArticoloInStock> doRetrieveAllInStock() throws SQLException{
 		Connection conn = null;
 		PreparedStatement stm = null;
@@ -98,6 +116,14 @@ public class ArticoloInStockModel {
 		return beans;
 	}
 	
+	
+	/**
+	 * Questo metodo si occupa di effettuare la ricerca di articoli in stock tramite
+	 * una stringa immessa dall'utente come parametro di ricerca.
+	 * @param daCercare un oggetto daCercare di tipo <strong>String</strong>
+	 * @return una lista di articoli in stock di tipo <strong>ArticoloInStock</strong>, altrimenti null.
+	 * @throws SQLException
+	 */
 	public List<ArticoloInStock> doRetrieveSimpleSearch(String daCercare) throws SQLException{
 		String query = "select * from "+TABLE_NAME_CATALOGO + " where (Modello LIKE ?) or (Marca LIKE ?) or (Descrizione LIKE ?)";
 		
@@ -144,12 +170,24 @@ public class ArticoloInStockModel {
 		return beans;
 	}
 	
-	public List<ArticoloInStock> doRetrieveAdvancedSearch(String daCercare, String marcaDaCercare, double minPrice, double maxPrice, String colore) throws SQLException{
+	
+	/**
+	 * Questo metodo si occupa di effettuare la ricerca di prodotti che matchano con i vari parametri
+	 * immessi dall'utente nell'apposito form.
+	 * @param daCercare un oggetto daCercare di tipo <strong>String</strong>
+	 * @param marca un oggetto marca di tipo <strong>String</strong>
+	 * @param minPrice un oggetto minPrice di tipo <strong>double</strong>
+	 * @param maxPrice un oggetto maxPrice di tipo <strong>double</strong>
+	 * @param colore un oggetto colore di tipo <strong>String</strong>
+	 * @return una lista di articoli in stock di tipo <strong>ArticoloInStock</strong>, altrimenti null.
+	 * @throws SQLException
+	 */
+	public List<ArticoloInStock> doRetrieveAdvancedSearch(String daCercare, String marca, double minPrice, double maxPrice, String colore) throws SQLException{
 		String query = "select * from "+TABLE_NAME_CATALOGO + " where ((Modello LIKE ?) or (Marca LIKE ?) or (Descrizione LIKE ?))"
 				+ " and (Prezzo >= ? and Prezzo <= ?) and (Marca LIKE ?) and ((Modello LIKE ?) or (Descrizione LIKE ?))";
 		
 		daCercare = "%"+daCercare+"%";
-		marcaDaCercare = "%"+marcaDaCercare+"%";
+		marca = "%"+marca+"%";
 		colore = "%"+colore+"%";
 		
 		Connection conn = null;
@@ -164,7 +202,7 @@ public class ArticoloInStockModel {
 			stm.setString(3, daCercare);
 			stm.setDouble(4, minPrice);
 			stm.setDouble(5, maxPrice);
-			stm.setString(6, marcaDaCercare);
+			stm.setString(6, marca);
 			stm.setString(7, colore);
 			stm.setString(8, colore);
 			
@@ -175,7 +213,7 @@ public class ArticoloInStockModel {
 			while(rs.next()) {
 				String codice = rs.getString("Codice");
 				String modello = rs.getString("Modello");
-				String marca = rs.getString("Marca");
+				String marca1 = rs.getString("Marca");
 				String img1 = rs.getString("Img1");
 				String img2 = rs.getString("Img2");
 				String img3 = rs.getString("Img3");
@@ -183,7 +221,7 @@ public class ArticoloInStockModel {
 				Double prezzo = rs.getDouble("Prezzo");
 				int disponibilita = rs.getInt("Disponibilita");
 
-				beans.add(new ArticoloInStock(codice, modello, marca, img1, img2, img3, descrizione, prezzo, disponibilita));
+				beans.add(new ArticoloInStock(codice, modello, marca1, img1, img2, img3, descrizione, prezzo, disponibilita));
 			}
 
 			stm.close();

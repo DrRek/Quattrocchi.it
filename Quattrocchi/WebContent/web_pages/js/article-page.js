01150f30e2057e2062335b429d05bc3a18fa3f11
abbettino <albertinopalmiero@gmail.com>
2018-01-19 12:02:08 +0100

    sto curando la grafica dell'errore ma
    non capisco dove sia showError() quindi non capisco dove posso sovrascriverlo, il button ok non riesco a modificarlo @drrek

--- Quattrocchi/WebContent/web_pages/js/article-page.js
+++ Quattrocchi/WebContent/web_pages/js/article-page.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 			error : function(xhr, status, errorThrown) {
 				console.log(JSON.stringify(xhr));
 				console.log("AJAX error: " + status + ' : ' + errorThrown);
-				showError("Errore durante l'aggiunta al carrello! Se il problema persiste contattaci");
+				showError("<div class='alert alert-danger container'><h3>Errore durante l'aggiunta al carrello! Se il problema persiste contattaci<h3></div>");
 			},
 			success : function(responseText) {
 				$("a#count").html(parseInt($("a#count").html()) + 1);

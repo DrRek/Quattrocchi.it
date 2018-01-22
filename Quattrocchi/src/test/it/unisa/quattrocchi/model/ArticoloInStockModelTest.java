481f0b136559c25c5903f10689ef9b817d02628d
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-22 10:44:18 +0100

    Test Articolo In Stock Model

---
+++ Quattrocchi/src/test/it/unisa/quattrocchi/model/ArticoloInStockModelTest.java
@@ -0,0 +1,70 @@
+package test.it.unisa.quattrocchi.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import it.unisa.quattrocchi.entity.ArticoloInStock;
+import it.unisa.quattrocchi.model.ArticoloInStockModel;
+
+class ArticoloInStockModelTest {
+	
+	private static ArticoloInStockModel articoloInStockModel;
+	
+	static {
+		articoloInStockModel = new ArticoloInStockModel();
+	}
+
+	@Test
+	public void TestRicercaPerId() throws SQLException {
+		ArticoloInStock articolo = articoloInStockModel.doRetrieveByIdInStock("999");
+		assertNotNull(articolo);
+		assertEquals(articolo.getCodice(),"999");
+		assertEquals(articolo.getMarca(),"GreenVision");
+		assertEquals(articolo.getModello(),"Occhiali iGreen");
+		assertEquals(articolo.getDisponibilità(),10);
+		assertEquals(articolo.getPrezzo(),30);
+	}
+	
+	@Test
+	public void TestRicercaTutti() throws SQLException {
+		List<ArticoloInStock> articoli = articoloInStockModel.doRetrieveAllInStock();
+		assertNotNull(articoli);
+		assertEquals(articoli.size(),articoloInStockModel.doRetrieveAllInStock().size());
+		assertEquals(articoli.get(0).getCodice(),"994");
+		assertEquals(articoli.get(0).getModello(),"Spirit titanium");
+		assertEquals(articoli.get(1).getCodice(),"995");
+		assertEquals(articoli.get(1).getModello(),"649 Series PO7649S Vincent Gallo");
+		assertEquals(articoli.get(2).getCodice(),"996");
+		assertEquals(articoli.get(2).getModello(),"Flak Draft Prizm Golf sport");
+	}
+	
+	@Test
+	public void TestRicercaSemplice() throws SQLException {
+		List<ArticoloInStock> articoli = articoloInStockModel.doRetrieveSimpleSearch("");
+		assertNotNull(articoli);
+		assertEquals(articoli.size(),articoloInStockModel.doRetrieveAllInStock().size());
+		List<ArticoloInStock> ricercaSemplice = articoloInStockModel.doRetrieveSimpleSearch("sp");
+		assertNotNull(ricercaSemplice);
+		assertEquals(ricercaSemplice.size(),2);
+		assertEquals(ricercaSemplice.get(0).getCodice(),"994");
+		assertEquals(ricercaSemplice.get(0).getModello(),"Spirit titanium");
+		assertEquals(ricercaSemplice.get(1).getCodice(),"996");
+		assertEquals(ricercaSemplice.get(1).getModello(),"Flak Draft Prizm Golf sport");
+	}
+	
+	@Test
+	public void TestRicercaAvanzata() throws SQLException {
+		List<ArticoloInStock> ricercaAvanzata = articoloInStockModel.doRetrieveAdvancedSearch("sp","Lindberg", 50, 60, "nero");
+		assertNotNull(ricercaAvanzata);
+		assertEquals(ricercaAvanzata.size(),1);
+		assertEquals(ricercaAvanzata.get(0).getCodice(),"994");
+		assertEquals(ricercaAvanzata.get(0).getMarca(),"Lindberg");
+		assertEquals(ricercaAvanzata.get(0).getPrezzo(),55);
+		assertEquals(ricercaAvanzata.get(0).getDisponibilità(),60);
+		assertTrue(ricercaAvanzata.get(0).getDescrizione().contains("nero"));
+	}
+}

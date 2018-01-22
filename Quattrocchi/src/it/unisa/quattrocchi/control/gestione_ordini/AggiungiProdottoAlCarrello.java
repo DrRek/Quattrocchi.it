b6643fc473fd5a50190b7e8ec8777a1ed49a6508
DrRek <lucareccia@hotmail.it>
2018-01-22 16:02:02 +0100

    aggiungi prodotto al carrello e checkout precondition

--- Quattrocchi/src/it/unisa/quattrocchi/control/gestione_ordini/AggiungiProdottoAlCarrello.java
+++ Quattrocchi/src/it/unisa/quattrocchi/control/gestione_ordini/AggiungiProdottoAlCarrello.java
@@ -2,10 +2,14 @@ package it.unisa.quattrocchi.control.gestione_ordini;
 
 import java.util.HashMap;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
 import it.unisa.quattrocchi.entity.Acquirente;
 import it.unisa.quattrocchi.entity.ArticoloInStock;
 import it.unisa.quattrocchi.entity.Cart;
@@ -21,29 +25,37 @@ import it.unisa.quattrocchi.model.ArticoloInStockModel;
  * Questa classe è una servlet che si occupa di gestire l'aggiunta di un articolo al carrello.
  */
 public class AggiungiProdottoAlCarrello extends HttpServlet{
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	static ArticoloInStockModel articoloInStockModel = new ArticoloInStockModel();
 	static AcquirenteModel acquirenteModel = new AcquirenteModel();
 
 	/**
 	 * Questo metodo si occupa di effettuare l'aggiunta di un articolo al carrello.
-	 * @precondition articoloId != null e corrisponde ad un id di un articolo nel database.
+	 * @precondition 	La richiesta è asincrona.
+	 * 					articoloId != null e corrisponde ad un id di un articolo nel database.
 	 * @postcondition cart.size() == cart.size()+1 
 	 */
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			
-			String articoloId = request.getParameter("articoloId");
+
+			//Per controllare che la richiesta sia del tipo giusto
+			if(!"XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+				request.setAttribute("error", "Errore generato dalla richiesta! Se il problema persiste contattaci.");
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/welcome");
+				dispatcher.forward(request, response);
+				return;
+			}
+
 			Cart carrello = null;
 			Acquirente a = (Acquirente) request.getSession().getAttribute("acquirente");
 			if(a != null) {
 				carrello = a.getCart();
 			}
 			else {
-				
+
 				if(request.getSession().getAttribute("carrello")!= null) {
 					carrello = (Cart)request.getSession().getAttribute("carrello");
 				}
@@ -52,24 +64,48 @@ public class AggiungiProdottoAlCarrello extends HttpServlet{
 					request.getSession().setAttribute("carrello", carrello);
 				}
 			}
-			
-			ArticoloInStock articolo;
-			if(articoloId != null && !(articoloId.equals(""))) {
-				articolo = articoloInStockModel.doRetrieveByIdInStock(articoloId);
-				carrello.addArticle(articolo);
 
+			String idS = request.getParameter("articoloId");
+			if(idS==null || idS.equals("")) {
+				response.setContentType("application/json");
+				response.setHeader("Cache-Control", "no-cache");
+				response.getWriter().write(new Gson().toJson("Necessario fornire identificativo del prodotto."));
+				return;
+			}
+
+			int id=0;
+			try {
+				id = Integer.parseInt(idS);
+			} catch(Exception e) {
+				response.setContentType("application/json");
+				response.setHeader("Cache-Control", "no-cache");
+				response.getWriter().write(new Gson().toJson("Formato identificativo del prodotto non valido."));
+				return;
+			}
+
+			if(id!=0) {
+				ArticoloInStock articolo;
+				articolo = articoloInStockModel.doRetrieveByIdInStock(id);
+				if(articolo==null) {
+					response.setContentType("application/json");
+					response.setHeader("Cache-Control", "no-cache");
+					response.getWriter().write(new Gson().toJson("Nessun articolo trovato."));
+				}
+
+				carrello.addArticle(articolo);
 				if(a!= null) {
 					acquirenteModel.updateCart(a);
 				}
 			}
+
 		} catch (Exception e) {
-			
+
 			System.out.println("Errore in aggiungi prodotto al carrello");
 			e.printStackTrace();
 		}
 		return;
 	}
-	
+
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 		doGet(request, response);
--- Quattrocchi/src/it/unisa/quattrocchi/control/gestione_ordini/Checkout.java
+++ Quattrocchi/src/it/unisa/quattrocchi/control/gestione_ordini/Checkout.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
 import it.unisa.quattrocchi.entity.Acquirente;
 
 import it.unisa.quattrocchi.entity.CreditCard;
@@ -26,37 +29,94 @@ import it.unisa.quattrocchi.model.OrderModel;
 public class Checkout extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
-	
+
 	static OrderModel model = new OrderModel();
 	static AcquirenteModel acModel = new AcquirenteModel();
 	static ArticoloInOrderModel aInOrderModel = new ArticoloInOrderModel();
 
-	
+
 	/**
 	 * Questo metodo si occupa di effettuare la procedura di checkout.
-	 * @precondition usr != null, CreditCardID != null e ShippingAddress != null
-	 * 				ShippingAddressID e CreditCardID devono corrispondere ad un indirizzo ed una carta di usr.
+	 * @precondition 	La richiesta è sincrona.
+	 * 					usr != null, CreditCardID != null e ShippingAddress != null
+	 * 					ShippingAddressID e CreditCardID devono corrispondere ad un indirizzo ed una carta di usr.
 	 * @postcondition usr.getCart().size() == 0 e il numero degli ordini passati di usr è incrementato di uno. 
 	 */
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
 		try {
+
+			//Per controllare che la richiesta sia del tipo giusto
+			if("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+				response.setContentType("application/json");
+				response.setHeader("Cache-Control", "no-cache");
+				response.getWriter().write(new Gson().toJson("Errore generato dalla richiesta! Se il problema persiste contattaci."));
+				return;
+			}
+
 			Acquirente usr = (Acquirente) request.getSession().getAttribute("acquirente");
-			String CreditCardID = request.getParameter("CreditCardID");
-			String ShippingAddressID = request.getParameter("ShippingAddressID");
-			if(usr!=null && CreditCardID!=null && !CreditCardID.equals("") && ShippingAddressID!=null && !ShippingAddressID.equals("")) {
-				CreditCard cc = usr.checkCC(CreditCardID);
-				ShippingAddress sa = usr.checkSA(ShippingAddressID);
-				if(cc!=null && sa!=null) {
-					Order nuovo = new Order(usr, sa, cc);
-					model.createOrder(nuovo);
-					usr.resetCart();
-					acModel.updateCart(usr);
-				} else {
+			if(usr==null) {
+				request.setAttribute("error", "E' neccessario effettuare il login!");
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/login");
+				dispatcher.forward(request, response);
+				return;
+			}
+
+			String creditCardIDS = request.getParameter("CreditCardID");
+			if(creditCardIDS==null || creditCardIDS.equals("")) {
+				request.setAttribute("error", "E' neccessario scegliere una carta di credito valida!");
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/visualizza_checkout");
+				dispatcher.forward(request, response);
+				return;
+			}
+
+			int creditCardID=0;
+			try {
+				creditCardID = Integer.parseInt(creditCardIDS);
+			} catch(Exception e) {
+				request.setAttribute("error", "E' neccessario scegliere una carta di credito valida!");
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/visualizza_checkout");
+				dispatcher.forward(request, response);
+				return;
+			}
+
+			String shippingAddressIDS = request.getParameter("ShippingAddressID");
+			if(shippingAddressIDS==null || shippingAddressIDS.equals("")) {
+				request.setAttribute("error", "E' neccessario scegliere un indirizzo di spedizione valido!");
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/visualizza_checkout");
+				dispatcher.forward(request, response);
+				return;
+			}
+
+			int shippingAddressID=0;
+			try {
+				shippingAddressID = Integer.parseInt(shippingAddressIDS);
+			} catch(Exception e) {
+				request.setAttribute("error", "E' neccessario scegliere un indirizzo di spedizione valido!");
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/visualizza_checkout");
+				dispatcher.forward(request, response);
+				return;
+			}
+
+			if(creditCardID!=0 && shippingAddressID!=0) {
+				CreditCard cc = usr.checkCC(creditCardID);
+				ShippingAddress sa = usr.checkSA(shippingAddressID);
+				if(cc==null || sa==null) {
 					request.setAttribute("error", "Errore nella scelta della carta di credito o dell'indirizzo!");
-					RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/profilo");
+					RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/visualizza_checkout");
 					dispatcher.forward(request, response);
+					return;
 				}
+				
+				Order nuovo = new Order(usr, sa, cc);
+				model.createOrder(nuovo);
+				usr.resetCart();
+				acModel.updateCart(usr);
+
+				request.setAttribute("error", "L'ordine è stato sottomesso con successo!");
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/profilo");
+				dispatcher.forward(request, response);
+				return;
 			}
 		} catch (Exception e) {
 			System.out.println("Errore in Checkout:");
@@ -64,7 +124,7 @@ public class Checkout extends HttpServlet{
 		}
 		return;
 	}
-	
+
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 		doGet(request, response);

eb93ef723a26eb52e30537ace18245e03eba4b08
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-22 18:50:54 +0100

    Da rivedere tutti i test dopo i cambiamenti di luca

--- Quattrocchi/src/test/it/unisa/quattrocchi/model/ArticoloInStockModelTest.java
+++ Quattrocchi/src/test/it/unisa/quattrocchi/model/ArticoloInStockModelTest.java
@@ -20,9 +20,9 @@ class ArticoloInStockModelTest {
 
 	@Test
 	public void TestRicercaPerId() throws SQLException {
-		ArticoloInStock articolo = articoloInStockModel.doRetrieveByIdInStock("999");
+		ArticoloInStock articolo = articoloInStockModel.doRetrieveByIdInStock(999);
 		assertNotNull(articolo);
-		assertEquals(articolo.getCodice(),"999");
+		assertEquals(articolo.getCodice(),999);
 		assertEquals(articolo.getMarca(),"GreenVision");
 		assertEquals(articolo.getModello(),"Occhiali iGreen");
 		assertEquals(articolo.getDisponibilità(),10);
@@ -34,11 +34,11 @@ class ArticoloInStockModelTest {
 		List<ArticoloInStock> articoli = articoloInStockModel.doRetrieveAllInStock();
 		assertNotNull(articoli);
 		assertEquals(articoli.size(),articoloInStockModel.doRetrieveAllInStock().size());
-		assertEquals(articoli.get(0).getCodice(),"994");
+		assertEquals(articoli.get(0).getCodice(),994);
 		assertEquals(articoli.get(0).getModello(),"Spirit titanium");
-		assertEquals(articoli.get(1).getCodice(),"995");
+		assertEquals(articoli.get(1).getCodice(),995);
 		assertEquals(articoli.get(1).getModello(),"649 Series PO7649S Vincent Gallo");
-		assertEquals(articoli.get(2).getCodice(),"996");
+		assertEquals(articoli.get(2).getCodice(),996);
 		assertEquals(articoli.get(2).getModello(),"Flak Draft Prizm Golf sport");
 	}
 	
@@ -50,9 +50,9 @@ class ArticoloInStockModelTest {
 		List<ArticoloInStock> ricercaSemplice = articoloInStockModel.doRetrieveSimpleSearch("sp");
 		assertNotNull(ricercaSemplice);
 		assertEquals(ricercaSemplice.size(),2);
-		assertEquals(ricercaSemplice.get(0).getCodice(),"994");
+		assertEquals(ricercaSemplice.get(0).getCodice(),994);
 		assertEquals(ricercaSemplice.get(0).getModello(),"Spirit titanium");
-		assertEquals(ricercaSemplice.get(1).getCodice(),"996");
+		assertEquals(ricercaSemplice.get(1).getCodice(),996);
 		assertEquals(ricercaSemplice.get(1).getModello(),"Flak Draft Prizm Golf sport");
 	}
 	
@@ -61,7 +61,7 @@ class ArticoloInStockModelTest {
 		List<ArticoloInStock> ricercaAvanzata = articoloInStockModel.doRetrieveAdvancedSearch("sp","Lindberg", 50, 60, "nero");
 		assertNotNull(ricercaAvanzata);
 		assertEquals(ricercaAvanzata.size(),1);
-		assertEquals(ricercaAvanzata.get(0).getCodice(),"994");
+		assertEquals(ricercaAvanzata.get(0).getCodice(),994);
 		assertEquals(ricercaAvanzata.get(0).getMarca(),"Lindberg");
 		assertEquals(ricercaAvanzata.get(0).getPrezzo(),55);
 		assertEquals(ricercaAvanzata.get(0).getDisponibilità(),60);

fdcfab3572385c52f78a425c61924e009155d909
nico95v <nico95volpe@gmail.com>
2018-01-23 15:00:51 +0100

    Eliminato controllo nella Servlet

--- Quattrocchi/WebContent/web_pages/js/ordini.js
+++ Quattrocchi/WebContent/web_pages/js/ordini.js
@@ -1,8 +1,5 @@
 function checkForErrorGestioneOrdine(){
 	var stato = $("input[name='statoOrdine']:checked");
-	var dataDiConsegna = $("input[name='dataDiConsegna']");
-	var dataDiConsegnaParsed = new Date(dataDiConsegna);
-	var dataDiOggi = new Date();
 	
 	if(!/^([A-Za-z]{3,10})$/.test($('select[name=corriere]').val())){
 		showError("Inserire un valore valido per il corriere!");
@@ -16,11 +13,6 @@ function checkForErrorGestioneOrdine(){
 	
 	if(!stato.val().equals("Da spedire") && !stato.val().equals("In corso") && !stato.val().equals("consegnato")){
 		showError("Inserire un valore valido per lo stato dell'ordine!");
-		return false;
-	}
-	
-	if(dataDiConsegna.getTime() <= dataDiOggi.getTime()) {
-		showError("Inserire un valore valido per la data di consegna dell'ordine!");
 		return false;
 	}
 

b9a9375d688c44682c0843ebed311680844c0e79
abbettino <albertinopalmiero@gmail.com>
2018-01-25 21:38:08 +0100

    notification fix

--- Quattrocchi/WebContent/web_pages/js/header.js
+++ Quattrocchi/WebContent/web_pages/js/header.js
@@ -1,19 +1,8 @@
 function showNotification(notification){
 	if(notification != null && notification != ""){
 		$('#notificationInfoDiv').show();
-		$('#notificationInfoDiv').css("color", "#8a6d3b");
-		$('#notificationInfoDiv').css("background-color", "#fcf8e3");
-		$('#notificationInfoDiv').css("border-color", "#faebcc");
-		$('#notificationText').html(notification);
-	}
-}
-
-function showSuccess(notification){
-	if(notification != null && notification != ""){
-		$('#notificationInfoDiv').show();
-		$('#notificationInfoDiv').css("color", "#5c5c5c");
-		$('#notificationInfoDiv').css("background-color", "#88ff08");
-		$('#notificationInfoDiv').css("border-color", "#a5d71d");
+		$('#notificationInfoDiv').removeClass("alert-success");
+		$('#notificationInfoDiv').addClass("alert-warning");
 		$('#notificationText').html(notification);
 	}
 }
--- Quattrocchi/WebContent/web_pages/js/user.js
+++ Quattrocchi/WebContent/web_pages/js/user.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
 					$("input[name=cap]").val("");
 					$("input[name=provincia]").val("");
 					$("input[name=stato]").val("");
-					showSuccess("Indirizzo inserito con successo");
+					showNotification("Indirizzo inserito con successo");
 				} catch(exception){
 					showNotification(responseText)
 				}
@@ -79,7 +79,7 @@ $(document).ready(function() {
 					$("input[name=circuito]").val("");
 					$("input[name=scadenza]").val("");
 					$("input[name=cvv]").val("");
-					showSuccess("Carta aggiunta con successo");
+					showNotification("Carta aggiunta con successo");
 				} catch(exception){
 					showNotification(responseText)
 				}

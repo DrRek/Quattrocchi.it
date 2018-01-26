76172cf1b36bd850c8f213d4d9e83b49d18b7c72
DrRek <lucareccia@hotmail.it>
2018-01-26 16:12:56 +0100

    Controllo data

--- Quattrocchi/src/it/unisa/quattrocchi/control/gestione_utenti/InserireCarta.java
+++ Quattrocchi/src/it/unisa/quattrocchi/control/gestione_utenti/InserireCarta.java
@@ -38,6 +38,7 @@ public class InserireCarta  extends HttpServlet{
 	 * 					circuito != null && circuito.matches("[A-Za-z ]{4,20}"),
 	 * 					scadenza != null && scandenza.matches("[0-9]{2}[/]{1}[0-9]{4}"),
 	 * 					cvv != null && cvv.matches("[0-9]{3}")
+	 * 					scadenza >= data di oggi.
 	 * @postcondition Il numero di carta di credito associate all'utente loggato è incrementato di 1.
 	 */
 	@Override
@@ -98,6 +99,11 @@ public class InserireCarta  extends HttpServlet{
 				scadenzaDate = df.parse(scadenza);
 			} catch(Exception e) {
 				response.getWriter().write(new Gson().toJson("Formato scadenza non valido."));
+				return;
+			}
+			
+			if(!scadenzaDate.after(new Date())) {
+				response.getWriter().write(new Gson().toJson("Inserire una data maggiore della data di oggi."));
 				return;
 			}
 			

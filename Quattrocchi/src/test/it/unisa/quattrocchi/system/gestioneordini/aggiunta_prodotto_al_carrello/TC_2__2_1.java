86912c6657f85b21c2aaa26cd8f4f9443082db19
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-26 18:39:20 +0100

    Altri test di sistema

---
+++ Quattrocchi/src/test/it/unisa/quattrocchi/system/gestioneordini/aggiunta_prodotto_al_carrello/TC_2__2_1.java
@@ -0,0 +1,5 @@
+package test.it.unisa.quattrocchi.system.gestioneordini.aggiunta_prodotto_al_carrello;
+
+public class TC_2__2_1 {
+
+}
---
+++ Quattrocchi/src/test/it/unisa/quattrocchi/system/gestioneordini/checkout/TC_2__3_1.java
@@ -0,0 +1,34 @@
+package test.it.unisa.quattrocchi.system.gestioneordini.checkout;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TC_2__3_1 {
+	private WebDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testTC231() throws Exception {
+		driver.get("http://localhost:8080/Quattrocchi/visualizza_checkout");
+		driver.findElement(By.cssSelector("input.btn.btn-outline-secondary")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+}

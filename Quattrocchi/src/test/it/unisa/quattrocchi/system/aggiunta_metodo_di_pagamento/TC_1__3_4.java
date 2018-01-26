4f90c96c6b2b94766a0f680a687e37beb731c5b5
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-26 16:21:55 +0100

    Wait

---
+++ Quattrocchi/src/test/it/unisa/quattrocchi/system/aggiunta_metodo_di_pagamento/TC_1__3_4.java
@@ -0,0 +1,54 @@
+package test.it.unisa.quattrocchi.system.aggiunta_metodo_di_pagamento;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TC_1__3_4 {
+	private WebDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testTC134() throws Exception {
+		driver.get("http://localhost:8080/Quattrocchi/logout");
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.name("userid")).click();
+		driver.findElement(By.name("passid")).clear();
+		driver.findElement(By.name("passid")).sendKeys("Forzajuve");
+		driver.findElement(By.name("userid")).clear();
+		driver.findElement(By.name("userid")).sendKeys("AntosxA");
+		driver.findElement(By.name("submit")).click();
+		driver.findElement(By.linkText("Benvenuto, AntosxA")).click();
+		driver.findElement(By.name("numcc")).click();
+		driver.findElement(By.name("numcc")).clear();
+		driver.findElement(By.name("numcc")).sendKeys("4023145652321456");
+		driver.findElement(By.name("intestatario")).clear();
+		driver.findElement(By.name("intestatario")).sendKeys("Antonio Spera");
+		driver.findElement(By.name("circuito")).clear();
+		driver.findElement(By.name("circuito")).sendKeys("VIS");
+		driver.findElement(By.name("scadenza")).clear();
+		driver.findElement(By.name("scadenza")).sendKeys("06/2023");
+		driver.findElement(By.name("cvv")).clear();
+		driver.findElement(By.name("cvv")).sendKeys("456");
+		driver.findElement(By.name("addCard")).click();
+		driver.findElement(By.cssSelector("input.btn.btn-outline-secondary")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+}

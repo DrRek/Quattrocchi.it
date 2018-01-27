373bde38ff0099ea1bb6d18ee11c4b9d30bdb9e7
pluigi94 <32388688+pluigi94@users.noreply.github.com>
2018-01-27 10:33:08 +0100

    Quasi finito

---
+++ Quattrocchi/src/test/it/unisa/quattrocchi/system/gestionearticoli/visualizza_catalogo/TC_3__2_2_2.java
@@ -0,0 +1,34 @@
+package test.it.unisa.quattrocchi.system.gestionearticoli.visualizza_catalogo;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TC_3__2_2_2 {
+	private WebDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testTC3222() throws Exception {
+		driver.get("http://localhost:8080/Quattrocchi/");
+		driver.findElement(By.linkText("Catalogo")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+}

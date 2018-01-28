1cfd25d43a97d5822e159e59080ef589ff3d451f
luca <lucareccia@hotmail.it>
2018-01-28 12:20:04 +0100

    Sistemato codici in test plan e codice

---
+++ Quattrocchi/src/test/it/unisa/quattrocchi/system/gestioneordini/aggiunta_prodotto_al_carrello/TC_2_2__2.java
@@ -0,0 +1,36 @@
+package test.it.unisa.quattrocchi.system.gestioneordini.aggiunta_prodotto_al_carrello;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TC_2_2__2 {
+	private WebDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testTC222() throws Exception {
+		driver.get("http://localhost:8080/Quattrocchi/");
+		driver.findElement(By.linkText("Catalogo")).click();
+		driver.findElement(By.cssSelector("img[alt=\"pic\"]")).click();
+		//driver.findElement(By.cssSelector("input.btn.btn-outline-secondary")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+}
---
+++ Quattrocchi/src/test/it/unisa/quattrocchi/system/gestioneutenti/login/TC_1_1__5.java
@@ -0,0 +1,40 @@
+package test.it.unisa.quattrocchi.system.gestioneutenti.login;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TC_1_1__5 {
+	private WebDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testTC115() throws Exception {
+		driver.get("http://localhost:8080/Quattrocchi/");
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.name("userid")).click();
+		driver.findElement(By.name("userid")).clear();
+		driver.findElement(By.name("userid")).sendKeys("Expos");
+		driver.findElement(By.name("passid")).clear();
+		driver.findElement(By.name("passid")).sendKeys("sonoivan");
+		driver.findElement(By.name("submit")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+}
